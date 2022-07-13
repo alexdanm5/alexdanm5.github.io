8070ad91ee6b5a3bce9f250e60d5317b47f8eaf5
alexdanm5 <106381421+alexdanm5@users.noreply.github.com>
2022-07-13 17:49:28 +0300

    Add files via upload

---
+++ glopt/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.slider__wrapp').slick({
+        slidesToShow: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="logo/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="logo/right.png"></button>'
+    });
+});
